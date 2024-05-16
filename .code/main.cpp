a6d9b1111d8ecf7d9dcdeda35e2e6a018c994cae
Minnie Cao <144734148+MinnieMwooh@users.noreply.github.com>
2024-05-16 02:27:31 +0000

    add main

---
+++ .code/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const size_t BLOCK_SIZE = 16;
+vector<unsigned char> stringToBytes(string str);
+void toMatrix(vector<unsigned char> bytes);
+
+int main(){
+    //read file (16 char at a time)
+    //input string to 16 char
+    //add padding if needed
+    return 0;
+}
+
+//convert string to vector of char (16 bytes)
+vector<unsigned char> stringToBytes(string str){
+    vector<unsigned char> string_bytes(str.begin(), str.end());
+    size_t padding_length = BLOCK_SIZE - (string_bytes.size() % BLOCK_SIZE);
+    //add bytes (padding) for string that is shorter than 16 bytes
+    for (size_t i = 0; i < padding_length; i++)
+    {
+        string_bytes.push_back(padding_length);
+    }
+}
+
+void toMatrix(unsigned char matrix[4][4], vector<unsigned char>& bytes) {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            matrix[i][j] = bytes[4*i+j];
+        }
+    }
+}
