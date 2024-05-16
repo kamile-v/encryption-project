e06a25a134e0def93351a43d05d3379ef94ab392
Minnie Cao <144734148+MinnieMwooh@users.noreply.github.com>
2024-05-16 03:24:21 +0000

    update cypherKey

--- .code/aes.h
+++ .code/aes.h
@@ -7,6 +7,8 @@ const int BLOCK_SIZE = 4;
 extern unsigned char sBox[256];
 extern unsigned char rCon[BLOCK_SIZE][BLOCK_SIZE];
 
+extern unsigned char cypherKey[BLOCK_SIZE][BLOCK_SIZE];
+
 //helper for mixColumn()
 unsigned char galoisFieldMul(unsigned char c1, unsigned char c2);
 
@@ -15,5 +17,8 @@ void byteSub(unsigned char matrix[BLOCK_SIZE][BLOCK_SIZE]);
 void shiftRow(unsigned char matrix[BLOCK_SIZE][BLOCK_SIZE]);
 void mixColumn(unsigned char matrix[BLOCK_SIZE][BLOCK_SIZE]);
 void addRoundKey(unsigned char matrix[BLOCK_SIZE][BLOCK_SIZE], unsigned char key[BLOCK_SIZE][BLOCK_SIZE]);
+
+//key scheduling
+void getKey(unsigned char *key, int keylen, unsigned char[])
 
 #endif
